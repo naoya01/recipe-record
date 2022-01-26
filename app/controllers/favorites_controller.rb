aac3868294f09f6aebf6c01d741d470f40f286ca
naoya01 <naoya.take10.01@gmail.com>
2022-01-26 04:34:36 +0000

    comment

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,14 +1,26 @@
-class FavoritesController < ApplicationControllers
+class FavoritesController < ApplicationController
+  def index
+  end
+
+  def show
+    @user = current_user
+    @favorites = current_user.favorites
+  end
 
   def create
     @cooking = Cooking.find(params[:cooking_id])
     favorite = current_user.favorites.new(cooking_id: @cooking.id)
     favorite.save
+
+    # redirect_to request.referer
   end
 
   def destroy
     @cooking = Cooking.find(params[:cooking_id])
     favorite = current_user.favorites.find_by(cooking_id: @cooking.id)
     favorite.destroy
+
+    # redirect_to request.referer
+
   end
 end
