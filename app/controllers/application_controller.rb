25c5690714fe1870f632a6e0ab5e2d1a879d340d
naoya01 <naoya.take10.01@gmail.com>
2022-02-15 05:16:40 +0000

    ゲストログイン

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!,except: [:top,:usage]
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :ensure_guest_user, only: [:edit]
 
   def after_sign_in_path_for(resource)
       meals_path
@@ -10,10 +11,18 @@ class ApplicationController < ActionController::Base
     resource.update_without_password(params)
   end
 
+
+
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
         # 情報更新時にnicknameの取得を許可
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
+  def ensure_guest_user
+    if current_user.name == "guestuser"
+      redirect_to meals_path
+    end
   end
 end
